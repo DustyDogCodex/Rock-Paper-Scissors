973e24ce41754a6e63f6e666a12bb0f7d491786a
Varun Malaviya <varunmalaviya7@gmail.com>
2022-02-10 13:58:00 -0500

    Finally finished the JS script for a game with 5 rounds of RPS! Changed the loop to a while loop and fixed user prompt iteration issues!

--- script.js
+++ script.js
@@ -3,48 +3,79 @@ let choices =['Rock','Paper','Scissors'];
 function computerPlay() {
     let n = Math.floor(Math.random() * 3);
     let move = choices[n];
-    console.log(n);
     console.log(move);
     return move;
 }
-computerPlay()
 
-let playerMove = 'rock';
+// function for playing a round
 
-let computerSelection = computerPlay().toLowerCase();
-let playerSelection = playerMove.toLowerCase();
+let playerScore = 0;
+let compScore =0;
 
-function playRound(playerSelection,computerSelection) {
+function playRound() {
     
+    let playerMove = prompt('What is your choice of weapon?');
+    let computerSelection = computerPlay().toLowerCase();
+    let playerSelection = playerMove.toLowerCase();
+
     switch (playerSelection) {
 
         case 'rock':
             if (playerSelection == computerSelection) {
-                console.log('its a tie!');
+                return ('its a tie!');
             } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-                console.log('You lose! Paper beats Rock!');
+                ++compScore;
+                return ('You Lose! Paper beats Rock!');
             } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-                console.log('You win! Rock beats Scissors!')
+                ++playerScore;
+                return ('You Win! Rock beats Scissors!');
             }
         
         case 'paper':
             if (playerSelection == computerSelection) {
-                console.log('its a tie!');
+                return ('its a tie!');
             } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-                console.log('You Win! Paper beats Rock!');
+                ++playerScore;
+                return ('You Win! Paper beats Rock!');
             } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-                console.log('You lose! Scissors beats Paper!')
+                ++compScore;
+                return ('You Lose! Scissors beats Paper!');
             }
         
         case 'scissors':
             if (playerSelection == computerSelection) {
-                console.log('its a tie!');
+                return ('its a tie!');
             } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-                console.log('You Win! Scissors beats Paper!');
+                ++playerScore;
+                return ('You Win! Scissors beats Paper!');
             } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-                console.log('You Lose! Rock beats Scissors!')
+                ++compScore;
+                return ('You Lose! Rock beats Scissors!');
             }
     }        
 }
 
-playRound(playerSelection,computerSelection)
+function game () {
+    
+
+    while (playerScore < 5 && compScore < 5) {
+        
+        console.log(playRound());
+
+        console.log('Player Score is ' + playerScore);
+        console.log('Computer Score is ' + compScore);
+        
+    }
+
+    
+
+    if (playerScore > compScore) {
+        console.log("HUZZA! THE PLAYER WON!!");
+    } else if (playerScore < compScore) {
+        console.log("AW SHUCKS! OUR COMPUTER OVERLORD HAS EMERGED VICTORIOUS!!");
+    } else {
+        console.log("THE GAME IS TIED!!");
+    }
+}
+
+console.log(game());
